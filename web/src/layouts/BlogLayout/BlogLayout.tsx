a5bb50795db756014d5a4b9c1c05673ab9378b4b
Mendel Stillerman <menstiler@gmail.com>
2024-02-26 13:44:47 -0500

    removing current user from navbar

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -10,7 +10,7 @@ export const SearchContext = createContext(null)
 
 const BlogLayout = ({ children }) => {
   const [searchResult, setSearchResult] = useState(null)
-  const { logOut, isAuthenticated, currentUser } = useAuth()
+  const { logOut, isAuthenticated } = useAuth()
 
   const updateResults = (value) => {
     setSearchResult(value)
@@ -64,25 +64,6 @@ const BlogLayout = ({ children }) => {
                 </Link>
               )}
             </li>
-            {isAuthenticated && (
-              <li className="text-xs">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
-                <div className="absolute right-0 mr-1">{currentUser.email}</div>
-              </li>
-            )}
           </ul>
         </nav>
       </header>
